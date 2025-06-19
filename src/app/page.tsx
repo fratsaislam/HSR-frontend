ac3d9ce0f165aef4bbc84890c71ecee7376affab
fratsaislam <i_fratsa@estin.dz>
2025-06-19 22:26:49 +0100

    UI enhancing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import Booking from '../components/Booking';
 import About from '../components/About';
 import Rooms from '../components/Rooms';
 import Services from '../components/Services';
