7350a4150103cdc1cedf938f6fb85bf2bded78da
fratsaislam <i_fratsa@estin.dz>
2025-06-19 02:58:43 +0100

    Fixing errors

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -2,18 +2,19 @@
 
 import { useEffect, useState } from "react";
 import getEmailFromToken from "@/utils/getEmailFromToken";
+import { useAuth } from "@/utils/useAuth";
 
 export default function DashboardPage() {
   const [email, setEmail] = useState("");
 
   useEffect(() => {
-    const userEmail = getEmailFromToken();
+    const {user} = useAuth();
     setEmail(userEmail);
   }, []);
 
   return (
     <div>
-      <h1>Welcome {email || "Loading..."}</h1>
+      <h1>Welcome {user.username || "Loading..."}</h1>
     </div>
   );
 }
